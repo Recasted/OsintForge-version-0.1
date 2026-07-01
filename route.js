eeb699702a7df44341ff74710849dcb13a4992e2
Recasted <lijkvocht1@gmail.com>
2026-07-01 18:08:00 +0200

    0.1.0

---
+++ route.js
@@ -0,0 +1,6 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
+R.c("server/chunks/[externals]_next_dist_0iuj5m_._.js")
+R.c("server/chunks/[root-of-the-server]__0ihj8v0._.js")
+R.c("server/chunks/osint-forge__next-internal_server_app_favicon_ico_route_actions_0bx1mi0.js")
+R.m(94663)
+module.exports=R.m(94663).exports
